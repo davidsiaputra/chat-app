2aa26f380a8b8d416a05dc2e45790fece7baffcc
stanley.susanto <stanley.susanto@vizio.com>
2021-07-29 16:34:10 -0700

    update

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 const session = require("express-session");
+const receive = app();
 
 app.use(
   session({
@@ -18,50 +18,9 @@ mongoose
   })
   .then(() => {
     console.log("connected to DB");
-  });
-
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  hashedPw: {
-    type: String,
-    required: true,
-  },
 });
 
-const User = mongoose.model("User", UserSchema);
-
-app.post("/register", async (req, res, next) => {
-  const { username, password } = req.body;
-  // here, you can get the username and password from req.body in the actual site. Here, we will hardcode the values.
-
-  const hashedPw = await bcrypt.hash(password, 12);
-  const user = await User.create({ username, hashedPw });
-  await user.save();
-  return res.send(user);
-});
-
-app.post("/login", async (req, res, next) => {
-  // again here, you would get the username and password from a form in the actual site. Here, we hardcode the values again.
-  const username = "sample";
-  const password = "password";
-  const user = await User.findOne({ username });
-  const matchstatus = await bcrypt.compare(password, user.hashedPw);
-  if (matchstatus == true) {
-    console.log("logged in!");
-    req.session.user = user;
-    return res.send(user);
-  } else {
-    return res.send("Wrong ID or Password");
-  }
-});
-
-app.post("/logout", (req, res, next) => {
-  req.session.user = null;
-  return res.send("DONE");
-});
+receive.use(require("controller/user.js"));
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
